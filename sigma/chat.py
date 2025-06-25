3f96c98c6a892753fe63f87ca2a5a0cee7c52a3f
Haimonmon <beankor8@gmail.com>
2025-06-25 21:39:58 +0800

    Added chat head positions for prompter and bot and typing_animation also added for bot

--- sigma/chat.py
+++ sigma/chat.py
@@ -8,30 +8,23 @@ from pyfiglet import Figlet
 from rich.panel import Panel
 from rich.console import Console
 
-from .chat_saver import load_chat
-from .brain import generate_chat_head, generate_previous_chat_heads, generate_bot_response
+from .chat_saver import load_chat, load_keyword
+from .brain import generate_chat_head, generate_previous_chat_heads, generate_bot_response, tokenization
 
 console = Console()
 
 def display_header(header_text: str, sub_header_text: str, title_header_text: str) -> None:
-    """ pang displat lang ng header as introduction, can be either bot name, perfect with 5 letters maximum"""
+    """ pang display lang ng header as introduction, can be either bot name, perfect with 5 letters maximum"""
 
     figlet = Figlet(font='alligator')
     logo = figlet.renderText(header_text)
+    line_separator = "_" * 75
     
-    console.print(Panel.fit(f"[bold magenta] {logo} \n {"_" * 75} \n {sub_header_text} [/bold magenta]", border_style = "magenta", title = title_header_text, padding=1))
+    console.print(Panel.fit(f"[bold magenta] {logo} \n {line_separator} \n {sub_header_text} [/bold magenta]", border_style = "magenta", title = title_header_text, padding=1), justify = "center")
 
 
 def display_chat_session(prompter_name, bot_name, selected_chat_session: str) -> None:
     """ display option """
-
-    # * For a while 
-    introduction_messages = [
-        "Chat Ahead . . .   ",
-        " Just hate being existed -_-",
-        "Just ask bruhhh . . .   ",
-        " Not a smart ass, just ask 👌"
-    ]
 
     chat_session: List = load_chat(selected_chat_session)
     is_previous_chat_loaded: bool = False
@@ -40,25 +33,33 @@ def display_chat_session(prompter_name, bot_name, selected_chat_session: str) ->
         if len(chat_session) == 0:
             generate_chat_head(
                 role = "bot",
-                response = random.choice(introduction_messages),
+                response = random.choice(load_keyword()["greetings"]["response"]),
                 bot_name = bot_name,
                 auto_save = True,
-                file_name = selected_chat_session
+                file_name = selected_chat_session,
+                enable_typing_animation = True
             )
 
             chat_session: List = load_chat(selected_chat_session)
         elif len(chat_session) != 0 and not is_previous_chat_loaded:
-            generate_previous_chat_heads(file_name = selected_chat_session)
+            generate_previous_chat_heads(file_name = selected_chat_session, prompter_name = prompter_name, bot_name = bot_name)
             is_previous_chat_loaded: bool = True
        
         response: str = console.input("[bold orange1] [ 👑 You ] [/] : ")
 
-        generate_bot_response(prompt = response)
+        generate_bot_response(prompt = response, prompter_name = prompter_name, bot_name = bot_name)
 
-        if response in ["bye", "goodbye", "seeyah"]:
+        if say_goodbye(response):
             break
 
 
+def say_goodbye(response: str) -> None:
+    for keyword in tokenization(response):
+        if keyword in load_keyword()["farewell"]["keyword"]:
+            return True
+    return False
+    
+    
 def sigma_bot(your_name: str = "Hitler", bot_name = "Chad") -> None:
     """ Landing Page """
 
@@ -76,9 +77,8 @@ if __name__ == "__main__":
 # TODO:
 # ! Make sure the prompter will have a response always even before saving its chat head
 # ! Make sure to have a Automatic Text wrap on each chat heads
-# ! Make the prompter chat heads on the right side while bot chat heads is on the left
-# ! Make all of the components on center
 # ! Add loading screen for startup and prompt bot delay response
+# ! use levenshtein algorithm for keyword mispelled
 
 # * References:
 # * Rich Documentation: https: //rich.readthedocs.io/en/stable/console.html
