6bab061c02e471bec8dcc08bd301f8b8fa0a7e32
Haimonmon <beankor8@gmail.com>
2025-07-04 19:00:31 +0800

    Modified: Added tokenization and end_conversation func

--- sigma/__init__.py
+++ sigma/__init__.py
@@ -2,5 +2,5 @@
 ## S I G M A BOT
 """
 
-from .brain import ask, tokenization
+from .brain import ask, tokenization, is_end_conversation
 from .memory import get_knowledge, get_remembered_messages, remember_message
